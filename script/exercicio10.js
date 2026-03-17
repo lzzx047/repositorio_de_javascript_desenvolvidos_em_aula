1b9e9e94b5f6a2b8d3523e7bb8537d38563b1e1f
Luiz <luizmarquesfernando037@gmail.com>
2026-03-16 22:15:15 -0300

    atividades atualizadas renan

---
+++ script/exercicio10.js
@@ -0,0 +1,12 @@
+//Peça o sexo da pessoa (H para homem, M para mulher) e a altura. Use as fórmulas para calcular o peso ideal:
+const sexo = (prompt("Digite o sexo H ou M"))
+const altura = Number(prompt("Digite uma altura"))
+ 
+if (sexo === 'H') {
+    alert (72.7 * altura) (- 58)
+} else if (sexo === 'M'){
+    alert (62.1 * altura) - 44.7
+}else {
+    alert("erro")
+}
+ 
